58fa91f3e7bc3180cdaaabaf427e3c75ec415f83
Mario Fronza <mariofronza1@gmail.com>
2020-10-20 10:54:02 -0300

    feat: created authenticate user service

---
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -0,0 +1,66 @@
+import { injectable, inject } from 'tsyringe';
+import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserService {
+  private usersRepository: IUsersRepository;
+
+  private hashProvider: IHashProvider;
+
+  constructor(
+    @inject('UsersRepository')
+    usersRepository: IUsersRepository,
+    @inject('HashProvider')
+    hashProvider: IHashProvider,
+  ) {
+    this.usersRepository = usersRepository;
+    this.hashProvider = hashProvider;
+  }
+
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({ id: user.id, role: user.role }, secret, {
+      expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
