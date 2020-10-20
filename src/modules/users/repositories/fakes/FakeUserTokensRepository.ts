d1eff4c64bbd5fcff1d8ffa18d158b209caf2565
Mario Fronza <mariofronza1@gmail.com>
2020-10-20 10:54:15 -0300

    feat: created sessions controller

---
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,33 @@
+import { uuid } from 'uuidv4';
+
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+import User from '@modules/users/infra/typeorm/entities/User';
+import IUserTokensRepository from '../IUserTokensRepository';
+
+class FakeUserTokensRepository implements IUserTokensRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(user: User): Promise<UserToken> {
+    const userToken = new UserToken();
+    Object.assign(userToken, {
+      id: Math.random(),
+      token: uuid(),
+      user,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
+    return userToken;
+  }
+}
+
+export default FakeUserTokensRepository;
