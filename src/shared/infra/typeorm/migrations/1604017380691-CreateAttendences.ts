ef5537f615b580679cf0bfa78a6f27b440be98e5
Mario Fronza <mariofronza1@gmail.com>
2020-11-04 18:30:07 -0300

    chore: added first classes, subjects and attendences migrations

---
+++ src/shared/infra/typeorm/migrations/1604017380691-CreateAttendences.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAttendences1604017380691 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/shared/infra/typeorm/migrations/1604017427302-CreateFiles.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateFiles1604017427302 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
