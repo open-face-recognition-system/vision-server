18ddbfb5b5196bf7bb3b7ad08642f7ca9c13132d
Mario Fronza <mariofronza1@gmail.com>
2021-02-01 22:10:44 -0300

    chore: lbph main folder context

--- src/shared/container/providers/RecognitionProvider/implementations/LBPHProvider.ts
+++ src/shared/container/providers/RecognitionProvider/implementations/LBPHProvider.ts
@@ -7,7 +7,8 @@ class LBPHProvider implements IRecognitionProvider {
     photosPath: string[],
     subjectId: number,
   ): Promise<string> {
-    const trainingPaht = './src/shared/infra/opencv/training/lbph.py';
+    const mainFolder = process.env.RECOGNITION_CONTEXT;
+    const trainingPaht = `./${mainFolder}/shared/infra/opencv/training/lbph.py`;
     const pythonProcess = spawnSync('python', [
       trainingPaht,
       JSON.stringify(ids),
@@ -19,7 +20,8 @@ class LBPHProvider implements IRecognitionProvider {
   }
 
   public async recognize(id: number, photoPath: string): Promise<string> {
-    const recognizePath = './src/shared/infra/opencv/recognize/lbph.py';
+    const mainFolder = process.env.RECOGNITION_CONTEXT;
+    const recognizePath = `./${mainFolder}/shared/infra/opencv/recognize/lbph.py`;
     const pythonProcess = spawnSync('python', [
       recognizePath,
       photoPath,
